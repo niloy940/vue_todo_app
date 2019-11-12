b9eab5b62b582680c04b629a18abd01e83e69fd0
Niloy Quazi <niloy940@gmail.com>
2019-11-13 01:47:04 +0600

    creating frontend

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <todo-list/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    TodoList
   }
 }
 </script>
 
 <style>
+ {
+  box-sizing: border-box;
+ }
+ .container {
+    max-width: 600px;
+    margin: 0 auto;
+ }
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +35,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24px;
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
 </style>
